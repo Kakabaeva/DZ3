4a9c28a9d285dd444d694c40fd12648f9f4b884e
Эмина Какабаева <eminakakabaeva@ws-192-168-1-43.st-net.ru>
2023-03-25 01:48:50 +0300

    DZ2_2

--- Program.cs
+++ Program.cs
@@ -4,25 +4,25 @@
 // 12821 -> да
 // 23432 -> да
 
-Console.WriteLine("Введите целое пятизначное число");
-int num = Convert.ToInt32(Console.ReadLine());
-CheckPal(num);
+// Console.WriteLine("Введите целое пятизначное число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// CheckPal(num);
  
-void CheckPal(int num)
-{
-if (num > 9999 && num < 100000)
-{
-int num1 = (num / 10000) % 10;
-int num2 = (num / 1000) % 10;
-int num4 = (num / 10) % 10;
-int num5 = num % 10;      
-if (num1 == num5 && num2 == num4)
-Console.WriteLine("Да");
-else
-Console.WriteLine("Нет");
-}    
+// void CheckPal(int num)
+// {
+// if (num > 9999 && num < 100000)
+// {
+// int num1 = (num / 10000) % 10;
+// int num2 = (num / 1000) % 10;
+// int num4 = (num / 10) % 10;
+// int num5 = num % 10;      
+// if (num1 == num5 && num2 == num4)
+// Console.WriteLine("Да");
+// else
+// Console.WriteLine("Нет");
+// }    
 
     
  
-else Console.WriteLine("Введено некорректное значение");
-}
+// else Console.WriteLine("Введено некорректное значение");
+// }
---
+++ Program2.cs
@@ -0,0 +1,26 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+ 
+// Console.WriteLine("Введите координату X первой точки");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату Y первой точки");
+// int y1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату Z первой точки");
+// int z1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату X второй точки");
+// int x2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату Y второй точки");
+// int y2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату Z второй точки");
+// int z2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Расстояние между точками = " + Math.Round(Dist(x1, x2, y1, y2, z1, z2),2));
+ 
+// double Dist(int x1, int x2, int y1, int y2, int z1, int z2)
+// {
+ 
+//     double result = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) +Math.Pow((z2 - z1), 2));
+//     return result;
+ 
+// }
