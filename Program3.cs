82b7bce893ab6299b0c051a056afb944d57f984f
Эмина Какабаева <eminakakabaeva@ws-192-168-1-43.st-net.ru>
2023-03-25 01:51:06 +0300

    DZ3_3

---
+++ Program3.cs
@@ -0,0 +1,16 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+ 
+Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+Cubed(num);
+ 
+void Cubed(int num)
+{
+for (int i=1; i<=num; i++)
+{
+    Console.Write((i*i*i)+" ");
+}
+}
